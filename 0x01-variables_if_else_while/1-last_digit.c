02b450f16c0ebf3f3d1ff519542eb79981150991
BILLKOLOS <billooko2@gmail.com>
2022-12-09 15:03:37 -0800

    lasty

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -9,18 +9,26 @@
  */
 int main(void)
 {
-        int n;
-	int last;
+	int n;
+	int num;
 
         srand(time(0));
-	n= rand() - RAND_MAX /2;
-	last = n % 10;
-        if (n>5)
-                printf("Last digit of %i is %i and is greater than 5\n", n, last);
-        else if (n==0)
-                printf("Last digit of %i is %i and is 0\n", n, last);
-        else if (last > 6)
-		printf("Last digit of %i is %i and is less than 6 and not 0\n", n, last);
+	n= rand() - RAND_MAX / 2;
+	
+        printf("Last digit of %d is ", n);
+	num = n % 10;
+	if (num > 5)
+	{	
+                printf("%d and is greater than 5\n", num);
+	}	
+        else if ((num <6) && (num < 0))
+	{
+		printf("%d and is less than 6 and not 0\n", num);
+	}
+	else
+	{		
+	        printf("%d and is 0\n", num);
+	}	
 
         return (0);
 }
