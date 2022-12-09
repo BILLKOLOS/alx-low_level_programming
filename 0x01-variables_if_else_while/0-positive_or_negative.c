6b3511b3ca382916adef2f6e31ff0372c400ea2a
BILLKOLOS <billooko2@gmail.com>
2022-12-09 13:39:58 -0800

    The CONDTIONALS

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,9 +13,9 @@ int main(void)
 
 	srand(time(0));
 	n=rand() - RAND_MAX /2;
-	if (n > 0)
+	if (n>0)
 		printf("%i is positive\n", n);
-	else if (n < 0)
+	else if (n<0)
 		printf("%i is negative\n", n);
 	else
 		printf("%i is zero\n", n);
