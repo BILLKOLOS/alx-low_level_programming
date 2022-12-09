e0bda127bed53a7f13b583f1c1008e487ffab25b
BILLKOLOS <billooko2@gmail.com>
2022-12-09 15:46:48 -0800

    CONDITIONALSIN C

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -7,17 +7,24 @@
  * and if it is positive,negative, or zero
  * Return: 0
  */
+
 int main(void)
 {
 	int n;
 
 	srand(time(0));
-	n=rand() - RAND_MAX /2;
-	if (n>0)
-		printf("%i is positive\n", n);
-	else if (n<0)
-		printf("%i is negative\n", n);
-	else
-		printf("%i is zero\n", n);
+	n= rand() - RAND_MAX /2;
+	if (n > 0)
+	{			
+		printf("%d is positive\n", n);
+	}
+	else if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	}
+	else if (n <0)
+	{
+		printf("%d is negative\n", n);
 	return (0);
+	}
 }
