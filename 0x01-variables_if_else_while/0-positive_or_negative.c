d7f0b02824e00cc6afc9053d13bd082071c2146f
BILLKOLOS <billooko2@gmail.com>
2022-12-12 21:52:24 -0800

    TASKONE1

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -10,11 +10,11 @@
 
 int main(void)
 {
-	int n;
+int n;
 	
-	srand(time(0));
-	n= rand() - RAND_MAX / 2;
-	if (n > 0)
+srand(time(0));
+n=rand() - RAND_MAX / 2;
+if (n > 0)
 	{			
 		printf("%d is positive\n", n);
 	}
