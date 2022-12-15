d7ca25695fe43dfa4a08773e9c61d012eacc6143
BILLKOLOS <billooko2@gmail.com>
2022-12-15 15:59:33 -0800

    task4

--- 0x04-more_functions_nested_loops/8-main.c
+++ 0x04-more_functions_nested_loops/8-main.c
@@ -1,3 +1,5 @@
+#include "main.h"
+
 /**
  * main - check the code
  *
@@ -5,8 +7,8 @@
  */
 int main(void)
 {
-	print_square(2);
-	print_square(10);
-	print_square(0);
-	return (0);
+    print_square(2);
+    print_square(10);
+    print_square(0);
+    return (0);
 }
