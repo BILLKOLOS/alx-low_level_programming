8f6662ad3b51d1cb236ba913fdc5a4452be8a451
BILLKOLOS <billooko2@gmail.com>
2022-12-15 01:11:26 -0800

    alpha

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -13,7 +13,7 @@ void print_alphabet(void)
 
 	while (c <= 'z')
 	{
-		putchar(c);
+		_putchar(c);
 		c++;
 	}
 
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,7 +1,7 @@
 #ifndef HOLBERTON_H_INCLUDED
 #define HOLBERTON_H_INCLUDED
 
-int putchar(char);
+int putchar(int);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
