65cf5eb984ec1c208307892ea073731ef6d10c0a
BILLKOLOS <billooko2@gmail.com>
2022-12-15 03:03:26 -0800

    task1

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,7 +1,7 @@
 #ifndef HOLBERTON_H_INCLUDED
 #define HOLBERTON_H_INCLUDED
 
-int putchar(int);
+int _putchar(char);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
