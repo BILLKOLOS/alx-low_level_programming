eacd4ff60937893c0cebd960db6964036770fb6e
BILLKOLOS <billooko2@gmail.com>
2022-12-15 07:19:45 -0800

    task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,12 +1,27 @@
 #include "main.h"
 
 /**
- * main - check the code
+ * jack_bauer - prints every minute of the day
  *
  * Return: Always 0.
  */
-int main(void)
+
+void jack_bauer(void)
+
 {
-	jack_bauer();
-	return (0);
+	int a;
+	int b;
+
+	for (a = 0; a <= 23; a++)
+	{
+		for (b = 0; b <= 59; b++)
+		{
+			_putchar (a / 10 + '0');
+			_putchar (a % 10 + '0');
+			_putchar (':');
+			_putchar (b / 10 + '0');
+			_putchar (b % 10 + '0');
+			_putchar ('\n');
+		}
+	}
 }
