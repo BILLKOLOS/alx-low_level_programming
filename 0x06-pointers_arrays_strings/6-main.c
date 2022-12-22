cf6aa9236feedbcf0a6c2b0c8fd52031d8e7d9fb
BILLKOLOS <billooko2@gmail.com>
2022-12-22 03:19:37 -0800

    rite a function that capitalizes all words of a string.

---
+++ 0x06-pointers_arrays_strings/6-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char str[] = "Capitalize on comes.\nhello world!\thello world.hello world\n";
+	char *ptr;
+
+	ptr = cap_string(str);
+	printf("%s", ptr);
+	printf("%s", str);
+	return (0);
+}
